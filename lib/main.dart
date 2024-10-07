5b7d0d00e288e978941d75ee242976b0e9f62f5f
Corentin Delot <corentindelot05@gmail.com>
2024-10-07 15:22:17 +0200

    commit initial

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('2048'),
+          backgroundColor: Colors.yellow,
+        ),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4, // 4 colonnes
+            childAspectRatio: 1, // Chaque cellule est carrée
+            padding: const EdgeInsets.all(20),
+          ),
+          itemCount: 16, // 4x4 = 16 cellules
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.all(4), // Espacement entre les cellules
+              height: 50.0,
+              color: Colors.orange,
+              alignment: Alignment.center,
+              child: Text(
+                'Cellule $index',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
